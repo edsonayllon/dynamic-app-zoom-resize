7e29be08ede0fd32ab46fa335299e6cf856f6234
Edson Ayllon <eayllon1@gmail.com>
2019-12-18 18:56:36 -0500

    Use inline css

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,16 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-document.body.style['transform-origin'] = `0 0`;
-
 function App() {
   const [scaleState, setScaleState] = useState(1);
 
   const addScale = () => {
     let newScale = scaleState + 0.1
-    document.body.style.transform = `scale(${newScale})`;
-    document.body.style.width = `${100*(1/newScale)}%`
     setScaleState(newScale);
   }
 
   const minusScale = () => {
     let newScale = scaleState - 0.1
-    document.body.style.transform = `scale(${newScale})`;
-    document.body.style.width = `${100*(1/newScale)}%`
     setScaleState(newScale);
   }
 
@@ -25,7 +19,11 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style ={{
+        transform: `scale(${scaleState})`,
+        transformOrigin: '0 0',
+        width: `${100*(1/scaleState)}%`
+      }}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
